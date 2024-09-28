d8c5eccb57ca938f8f0a9e4ec4fd5397d9edc87d
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-09-28 12:54:34 -0300

    Organização em um arquivo

--- assets/lib/audioContext/simple-tracker.js
+++ assets/lib/audioContext/simple-tracker.js
@@ -186,6 +186,64 @@ class tracker {
   }
 }
 
+var _colId;
+
+const defaultTrack = {
+  beat: [
+    { rowId: '0', colId: '0', enabled: false },
+  ],
+  settings: {
+    // sampleSet: '../assets/audio/studio/samples.json',
+    sampleSet: 'https://roneicostasoares.com.br/Orgao/assets/audio/studio/samples.json',
+    measureLength: 16,
+    bpm: 90,
+    detune: 0,
+    gainEnabled: 'gain',
+    attackAmp: 0,
+    sustainAmp: 0.4,
+    decayAmp: 0.7,
+    releaseAmp: 1,
+    attackTime: 0,
+    decayTime: 0,
+    sustainTime: 2,
+    releaseTime: 2,
+    adsrInterval: 0.1,
+    delay: 0.01,
+    filter: 1000,
+  },
+};
+
+function initializeSampleSet(ctx, dataUrl, track) {
+  var sampleSetPromise = loadSampleSet(ctx, dataUrl);
+  sampleSetPromise.then(function (data) {
+    buffers = data.buffers;
+    sampleData = data.data;
+
+    if (!track) {
+      track = storage.getTrack();
+    }
+
+    if (!track.settings.measureLength) {
+      track.settings.measureLength = 16;
+    }
+
+    currentSampleData = sampleData;
+    setupTrackerHtml(sampleData, track.settings.measureLength);
+    schedule.loadTrackerValues(track.beat);
+    schedule.setupEvents();
+  });
+}
+
+window.onload = function () {
+  ctx = new AudioContext();
+  schedule = new tracker(ctx, scheduleAudioBeat);
+  getSetAudioOptions.setTrackerControls(defaultTrack.settings);
+  initializeSampleSet(ctx, defaultTrack.settings.sampleSet, defaultTrack);
+  setupBaseEvents();
+};
+
+gerarRitmosNomes(ritmosNomes);
+
 function hasClass(el, str) {
   if (el.classList) {
     return el.classList.contains(str);
