d36b11d1863e2bd0b300e69aa171751101d11080
Ronei Costa Soares <ronei.costa@aplicativo.net>
2025-05-07 11:30:31 -0300

    ajuste nos níveis de volume para os sons do órgão e das strings

--- assets/js/scriptNew.js
+++ assets/js/scriptNew.js
@@ -383,9 +383,9 @@ class CifraPlayer {
                 this.adicionarSomAoGrupo('strings', nota.replace('#', '_'), 'baixo', 0.4);
     
             if (this.elements.notesButton.classList.contains('pressed')) {
-                this.adicionarSomAoGrupo('orgao', nota.replace('#', '_', 0.9));
+                this.adicionarSomAoGrupo('orgao', nota.replace('#', '_', 0.7));
                 if (!this.elements.notesButton.classList.contains('notaSolo'))
-                    this.adicionarSomAoGrupo('strings', nota.replace('#', '_', 0.1));
+                    this.adicionarSomAoGrupo('strings', nota.replace('#', '_', 0.4));
             }
         });
     
