8ce95bab59887aef3fd3febc39b8d19771d3d4c7
Lucas <lucas@lucas.com>
2025-09-15 12:19:29 -0300

    correção

--- assets/js/scriptNew.js
+++ assets/js/scriptNew.js
@@ -1672,7 +1672,7 @@ function salvarSave(newSaveName) {
 document.addEventListener('DOMContentLoaded', () => {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('./sw.js')
+            navigator.serviceWorker.register('/sw.js')
                 .then(registration => {
                     console.log('Service Worker registrado com sucesso:', registration.scope);
                 })
