4ceea0c45e1928c9bc9c42c8a6b6d10c4c4e9701
Ronei Costa Soares <ronei.costa@aplicativo.net>
2025-10-11 15:02:44 -0300

    remove arquivos desnecessários

--- assets/js/notes.js
+++
@@ -1,7 +0,0 @@
-const Notes = function (selector, tuner) {
-    this.tuner = tuner;
-    this.isAutoMode = false;
-    this.$root = document.querySelector(selector);
-    this.$notes = [];
-    this.$notesMap = {};
-};
