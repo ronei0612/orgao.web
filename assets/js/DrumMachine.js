f434202ffac5cc03a55c8c8e865b697563a8995b
Ronei Costa Soares <ronei.costa@aplicativo.net>
2025-10-15 20:46:18 -0300

    ajuste dos icones

--- assets/js/DrumMachine.js
+++ assets/js/DrumMachine.js
@@ -6,7 +6,7 @@ class DrumMachine {
         this.instruments = [
             { name: 'Prato', icon: 'prato.svg', file: audioPath + 'ride.ogg', file3: audioPath + 'prato2.ogg' },
             { name: 'Tom', icon: 'tom.svg', file: audioPath + 'tom-03.ogg', file3: audioPath + 'tom-02.ogg' },
-            { name: 'Surdo', icon: 'tom.svg', file: audioPath + 'tom.ogg', file3: audioPath + 'prato1.ogg' },
+            { name: 'Surdo', icon: 'surdo.svg', file: audioPath + 'tom.ogg', file3: audioPath + 'prato1.ogg' },
             { name: 'Chimbal', icon: 'chimbal.svg', file: audioPath + 'chimbal.ogg', file3: audioPath + 'aberto.ogg' },
             { name: 'Caixa', icon: 'caixa.svg', file: audioPath + 'caixa.ogg', file3: audioPath + 'aro.ogg' },
             { name: 'Bumbo', icon: 'bumbo.svg', file: audioPath + 'bumbo.ogg', file3: null },
