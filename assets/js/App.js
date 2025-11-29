5419431d7e442ef4db04f2ba88f3e030a027185e
Ronei Costa Soares <ronei.costa@aplicativo.net>
2025-11-29 20:43:12 -0300

    inverte svg

--- assets/js/App.js
+++ assets/js/App.js
@@ -350,12 +350,12 @@ class App {
 
     handleOrgaoInstrumentClick() {
         if (this.cifraPlayer.instrumento === 'orgao') {
-            this.elements.orgaoInstrumentButton.firstElementChild.src = './assets/icons/piano.svg';
+            this.elements.orgaoInstrumentButton.firstElementChild.src = './assets/icons/teclado.svg';
             this.cifraPlayer.instrumento = 'epiano';
             this.cifraPlayer.attack = 0;
         }
         else {
-            this.elements.orgaoInstrumentButton.firstElementChild.src = './assets/icons/teclado.svg';
+            this.elements.orgaoInstrumentButton.firstElementChild.src = './assets/icons/piano.svg';
             this.cifraPlayer.instrumento = 'orgao';
             this.cifraPlayer.attack = 0.2;
         }
