a6e6da18ca4691441ba5219f9045f62f50701775
Ronei Costa Soares <ronei.costa@aplicativo.net>
2025-12-01 17:41:22 -0300

    version

--- assets/js/App.js
+++ assets/js/App.js
@@ -8,7 +8,7 @@ class App {
         this.BASE_URL = location.origin.includes('file:') ? 'https://roneicostasoares.com.br/orgao.web' : '.';
         this.cifraPlayer = new CifraPlayer(this.elements, this.uiController, this.musicTheory, this.BASE_URL);
 
-        this.version = '4.1';
+        this.version = '4.2';
         this.holdTime = 1000;
         this.held = false;
         this.pesquisarNaWeb = false;
